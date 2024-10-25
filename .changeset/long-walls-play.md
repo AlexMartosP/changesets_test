7a9dad97aed0c6882a3b909b3a932e93ebcd3b8f
Alexander Martos Paschoal <alexander.martos@minut.com>
2024-10-25 22:19:26 +0200

    Remove a log

---
+++ .changeset/long-walls-play.md
@@ -0,0 +1,5 @@
+---
+"@alexmartosp/logger": minor
+---
+
+Remove a log
--- packages/logger/index.js
+++ packages/logger/index.js
@@ -3,7 +3,3 @@ import { divide, sum } from "@alexmartosp/calcer";
 export function logSum() {
   console.log(sum(2, 3));
 }
-
-export function logDivide() {
-  console.log(divide(2, 3));
-}
